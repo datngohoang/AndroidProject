450e16898074a5ffd825c241de9b7b547ced7e5b
KIETTTSE62520\KIETTTSE62520 <truongtokiet@gmail.com>
2018-07-03 22:19:34 +0700

    voca- listen layout[kiet]

--- app/src/main/java/hciproject/datnh/englishquiz/ListeningActivity.java
+++ app/src/main/java/hciproject/datnh/englishquiz/ListeningActivity.java
@@ -1,20 +1,17 @@
 package hciproject.datnh.englishquiz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class VocabularyActivity extends AppCompatActivity {
+public class ListeningActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vocabulary);
+        setContentView(R.layout.activity_listening);
     }
 
     public void backToMenu(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
     }
 }
