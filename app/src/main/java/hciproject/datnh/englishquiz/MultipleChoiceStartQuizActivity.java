81c296b0587bed72f25d1b7755d3aca4c2a51aad
DATNHSE62481\Dat09 <datngohoang>
2018-07-03 23:10:11 +0700

    Create time, total and current question text and add text to question space [Dat]

--- app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
+++ app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
+
+    private TextView txtQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice_start_quiz);
+
+        //set textview
+        txtQuestion = (TextView) findViewById(R.id.txtQuestion);
+        String s = "Mary is 30 years old. Which one of the following is correct? \n\n" +
+                "A. Mary is 19 years old.\n\n" +
+                "B. Mary is 30 years old.\n\n" +
+                "C. Mary is 15 years old.\n\n" +
+                "D. Mary is 19 years old.\n\n";
+        txtQuestion.setText(s);
     }
 
     public void backToMenu(View view) {
