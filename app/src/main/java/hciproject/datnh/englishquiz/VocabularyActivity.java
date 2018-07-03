71d4e0b37682164302a19464d3f75ab190fc2b50
KIETTTSE62520\KIETTTSE62520 <truongtokiet@gmail.com>
2018-07-03 21:13:39 +0700

    test create activity [kiet]

---
+++ app/src/main/java/hciproject/datnh/englishquiz/VocabularyActivity.java
@@ -0,0 +1,13 @@
+package hciproject.datnh.englishquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VocabularyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vocabulary);
+    }
+}
