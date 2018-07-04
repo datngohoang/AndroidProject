d13e079a349133154198c02d164fa3b7b2356c06
Sain <sivanguyensf@gmail.com>
2018-07-04 11:11:48 +0700

    backend-Multiple[Hung]

--- app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceActivity.java
+++ app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 
 public class MultipleChoiceActivity extends AppCompatActivity {
+
+    private Spinner spinQues = null;
+    private Spinner spinDiff = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice);
+
+        spinQues = (Spinner)findViewById(R.id.spinnerQues);
+        spinDiff = (Spinner)findViewById(R.id.spinnerDiff);
     }
 
     public void backToMenu(View view) {
@@ -20,6 +27,10 @@ public class MultipleChoiceActivity extends AppCompatActivity {
 
     public void doQuiz(View view) {
         Intent intent = new Intent(MultipleChoiceActivity.this, MultipleChoiceStartQuizActivity.class);
+        int numQues = Integer.parseInt(spinQues.getSelectedItem().toString());
+        String diff = spinDiff.getSelectedItem().toString();
+        intent.putExtra("numQues", numQues);
+        intent.putExtra("diff", diff);
         startActivity(intent);
     }
 }
--- app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
+++ app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
@@ -4,25 +4,77 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
 
     private TextView txtQuestion;
+    private TextView txtTime;
+    private TextView txtTotal;
+    private TextView txtCurrent;
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private int mScore = 0;
+    private int currentQues = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice_start_quiz);
-
-        //set textview
+        //get num of ques and difficulty
+        Bundle b = new Bundle();
+        b = getIntent().getExtras();
+        int numQues = b.getInt("numQues");//5, 10, 20, 40
+        String diff = b.getString("diff");//Easy, Normal, Hard
+        //set total
+        txtQuestion = (TextView) findViewById(R.id.txtTotal);
+        txtTotal.setText(numQues);
+        //set textview question
         txtQuestion = (TextView) findViewById(R.id.txtQuestion);
-        String s = "Mary is 30 years old. Which one of the following is correct? \n\n" +
-                "A. Mary is 19 years old.\n\n" +
-                "B. Mary is 30 years old.\n\n" +
-                "C. Mary is 15 years old.\n\n" +
-                "D. Mary is 19 years old.\n\n";
-        txtQuestion.setText(s);
+        txtCurrent = (TextView) findViewById(R.id.txtCurrent); 
+        setTextview();
+        //check answer
+        btnA = (Button)findViewById(R.id.btnA);
+        btnB = (Button)findViewById(R.id.btnB);
+        btnC = (Button)findViewById(R.id.btnC);
+        btnD = (Button)findViewById(R.id.btnD);
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //dao
+                //boolean = dao.checkMultipleResult();
+            }
+        });
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void setTextview(){
+        currentQues++;
+        //dao
+        //get and set ques
+        txtCurrent.setText("" + currentQues);
     }
 
     public void backToMenu(View view) {
