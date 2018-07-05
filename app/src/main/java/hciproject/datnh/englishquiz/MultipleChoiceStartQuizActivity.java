d32ebf3deaa5b24e8f383b82b23cfb1f68f61ba8
Sain <sivanguyensf@gmail.com>
2018-07-05 10:54:39 +0700

    backend-multiplechoice2

--- app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
+++ app/src/main/java/hciproject/datnh/englishquiz/MultipleChoiceStartQuizActivity.java
@@ -11,10 +11,8 @@ import org.w3c.dom.Text;
 
 public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
 
-    private String onChosing;
     private TextView txtQuestion;
     private TextView txtTime;
-
     private TextView txtCurrent;
     private Button btnA;
     private Button btnB;
@@ -23,19 +21,24 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
     private Button btnConfirm;
     private int mScore = 0;
     private int currentQues = 0;
+    private int time;
+    private int numQues;
+    private String diff;
+    private String onChosing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice_start_quiz);
         //get num of ques and difficulty
-        Bundle b = new Bundle();
-        b = getIntent().getExtras();
-        int numQues = b.getInt("numQues");//5, 10, 20, 40
-        String diff = b.getString("diff");//Easy, Normal, Hard
+        numQues = getIntent().getExtras().getInt("numQues");//5, 10, 20, 40
+        diff = getIntent().getExtras().getString("diff");//Easy, Normal, Hard
         //set total
         txtQuestion = (TextView) findViewById(R.id.txtTotal);
         txtQuestion.setText("Total: " + numQues);
+        //set time
+        time = calculateTime(numQues);
+
         //set textview question
         txtQuestion = (TextView) findViewById(R.id.txtQuestion);
         txtCurrent = (TextView) findViewById(R.id.txtCurrent); 
@@ -45,11 +48,13 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
         btnB = (Button)findViewById(R.id.btnB);
         btnC = (Button)findViewById(R.id.btnC);
         btnD = (Button)findViewById(R.id.btnD);
+        btnConfirm = (Button)findViewById(R.id.btnConfirm);
         btnA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //change color
                 onChosing = "a";
+                changeBgButton(onChosing);
             }
         });
         btnB.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +62,7 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //change color
                 onChosing = "b";
+                changeBgButton(onChosing);
             }
         });
         btnC.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +70,7 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //change color
                 onChosing = "c";
+                changeBgButton(onChosing);
             }
         });
         btnD.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +78,7 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //change color
                 onChosing = "d";
+                changeBgButton(onChosing);
             }
         });
         btnConfirm.setOnClickListener(new View.OnClickListener() {
@@ -78,12 +86,37 @@ public class MultipleChoiceStartQuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //dao
                 //boolean = dao.checkMultipleResult();
+                //mScore++;
             }
         });
     }
 
     private void changeBgButton(String choice) {
+        if (choice.equals("a")) {
+            btnA.setBackgroundResource(R.drawable.button_bg_round_chosen);
+            btnB.setBackgroundResource(R.drawable.button_bg_round);
+            btnC.setBackgroundResource(R.drawable.button_bg_round);
+            btnD.setBackgroundResource(R.drawable.button_bg_round);
+        } else if (choice.equals("b")) {
+            btnA.setBackgroundResource(R.drawable.button_bg_round);
+            btnB.setBackgroundResource(R.drawable.button_bg_round_chosen);
+            btnC.setBackgroundResource(R.drawable.button_bg_round);
+            btnD.setBackgroundResource(R.drawable.button_bg_round);
+        } else if (choice.equals("c")) {
+            btnA.setBackgroundResource(R.drawable.button_bg_round);
+            btnB.setBackgroundResource(R.drawable.button_bg_round);
+            btnC.setBackgroundResource(R.drawable.button_bg_round_chosen);
+            btnD.setBackgroundResource(R.drawable.button_bg_round);
+        } else if (choice.equals("d")) {
+            btnA.setBackgroundResource(R.drawable.button_bg_round);
+            btnB.setBackgroundResource(R.drawable.button_bg_round);
+            btnC.setBackgroundResource(R.drawable.button_bg_round);
+            btnD.setBackgroundResource(R.drawable.button_bg_round_chosen);
+        }
+    }
 
+    private int calculateTime(int numQues) {
+        return numQues * 60;
     }
 
     private void setTextview(){
